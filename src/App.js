1ecfd5766dc5f385c81dffbbfcbbb7eafdc597fd
Skiboybl <69096827+Skiboybl@users.noreply.github.com>
2020-09-19 02:16:45 +0100

    productions

--- src/App.js
+++ src/App.js
@@ -124,10 +124,10 @@ const App = () => {
       <Card className="app__right">
         <CardContent>
           <div className="app__information">
-            <h3>Live Cases by Country</h3>
+            <h3 className="app__graphTitle">Live Cases by Country</h3>
             <Table countries={tableData} />
             <h3>Worldwide new {casesType}</h3>
-            <LineGraph casesType={casesType} />
+            <LineGraph className="app__graph" casesType={casesType} />
           </div>
         </CardContent>
       </Card>
